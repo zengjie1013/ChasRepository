7303b3d60fbcfe57b8172af416d86772e825c867
chas <894555763@qq.com>
2017-02-16 14:20:55 +0800

    添加RSA签名，验证签名，公钥加密，私钥解密，私钥加密，公钥解密功能

---
+++ chas-common/src/main/java/com/chas/security/rsa/KeyEnum.java
@@ -0,0 +1,15 @@
+package com.chas.security.rsa;
+
+/**
+ * 密钥类型枚举，公钥和私钥
+ * @author Chas Zeng
+ * @date 2017/2/16
+ */
+public enum KeyEnum {
+    PUBLIC,
+    PRIVATE;
+
+    private KeyEnum(){
+
+    }
+}
