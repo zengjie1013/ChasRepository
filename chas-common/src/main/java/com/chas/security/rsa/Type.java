48434724f965baf6b1d131235231eea0084b9940
chas <894555763@qq.com>
2017-02-16 14:21:33 +0800

    添加加密和解密枚举

---
+++ chas-common/src/main/java/com/chas/security/rsa/Type.java
@@ -0,0 +1,16 @@
+package com.chas.security.rsa;
+
+/**
+ * 加密和解密枚举
+ *
+ * @author Chas Zeng
+ * @date 2017/2/16
+ */
+public enum Type {
+    ENCRYPT,
+    DECRYPT;
+
+    private Type(){
+
+    }
+}
